6b363d9338979f73fe5fd51ebcfad558aaf9bf3e
eademarco <emily.demarco@gmail.com>
2014-06-28 15:39:23 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spurty::Application.config.secret_key_base = '1157939b002e25744b4c0021c91b3f9214ae7a5e1c852d48f4cb71c93a7b27aeb6511f438299568868d909956362f5fc5233fca0fc412e2fba2e0305d057c0c7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Spurty::Application.config.session_store :cookie_store, key: '_spurty_session'
